9f7dabefdbe79fe30ba636c4fe7776558dd294d0
Ayden Armstrong <ayden.armstrong@wsu.edu>
2023-08-24 21:54:32 -0700

    Resolving dependency resolution for react 18

--- ETFBLEND/frontend/src/components/Home.js
+++ ETFBLEND/frontend/src/components/Home.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import AddETFPage from "./AddETFPage";
-
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from '@mui/material/TextField';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from "@mui/material/FormControl";
+import { Link } from "react-router-dom";
+import Table from '@mui/material/Table';
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 export default class Home extends Component {
     constructor(props) {
@@ -8,8 +16,10 @@ export default class Home extends Component {
     }
 
     render(){
-        return (
-            <p>This is the home page.</p>
-        );
+        return <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                
+            </Grid>
+        </Grid>
     }
 }
