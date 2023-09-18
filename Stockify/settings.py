fe79b2b702e3050315c16797b2792860730badbb
Ayden Armstrong <ayden.armstrong@wsu.edu>
2023-09-17 22:39:47 -0700

    Maybe.

--- Stockify/settings.py
+++ Stockify/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,14 +93,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Stockify.urls'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = Path(BASE_DIR).joinpath('staticfiles')
-STATICFILES_DIRS = (Path(BASE_DIR).joinpath('static'),)
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'frontend/dist/static'),
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'frontend/dist'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
